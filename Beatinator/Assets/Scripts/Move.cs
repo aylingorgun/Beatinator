04e18dfec3e57cd7bb3b63be31ad0e429d5a2771
Aylin Görgün <aylingrgn2@gmail.com>
2019-03-15 21:58:49 +0300

    Initial Commit

---
+++ Beatinator/Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Beatinator/Assets/Scripts/PlaySoundsOnBeat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundsOnBeat : MonoBehaviour
+{
+    public SoundManager _soundManager;
+    //Base voices metronome
+    public AudioClip _tap, _tick;
+    //Your library of sounds
+    public AudioClip[] _strum;
+    int _randomStrum;
+    //Your items that will be created according to your beat
+    public GameObject myPrefab1, myPrefab2;
+
+    void Update()
+    {
+        if (BPM._beatFull)
+        {
+            _soundManager.Playsound(_tap, 1);
+            if (BPM._beatCountFull % 2 == 0)
+            {
+                GameObject go = (GameObject)Instantiate(myPrefab1);
+                _randomStrum = Random.Range(0, _strum.Length);
+            }
+        }
+        if (BPM._beatD8 && BPM._beatcountD8 % 2 == 0)
+        {
+            _soundManager.Playsound(_tick, 0.1f);
+        }
+        if (BPM._beatD8 && (BPM._beatcountD8) % 8 == 2 || (BPM._beatcountD8) % 8 == 4)
+        {
+            GameObject go = (GameObject)Instantiate(myPrefab2);
+            _soundManager.Playsound(_strum[_randomStrum], 1);
+        }
+    }
+}
