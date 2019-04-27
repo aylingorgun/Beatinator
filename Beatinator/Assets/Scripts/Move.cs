f186d5e2f5d0acd9f9ed9bc1e3e75792d1b5d4c7
AlpA <alpars44@hotmail.com>
2019-04-27 11:02:28 +0300

    Align movement to "snap" to grid

--- Beatinator/Assets/Scripts/Move.cs
+++ Beatinator/Assets/Scripts/Move.cs
@@ -4,28 +4,28 @@ using UnityEngine.SceneManagement;
 
 public class Move : MonoBehaviour
 {
-
-    public float speed = 1.5f;
-    public float step = 5f;
+    //Get rid of speed?
+    public float speed = 1f;
+    public float step = 2f;
 
     void Update()
     {
       
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime * step;
+            transform.position += Vector3.left * speed *  step;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime * step;
+            transform.position += Vector3.right * speed  * step;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime * step;
+            transform.position += Vector3.forward * speed  * step;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime * step;
+            transform.position += Vector3.back * speed  * step;
         }
 
     }
